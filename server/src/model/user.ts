4dbff8cc2cd9379e73e5d587825831902542a76b
yashverma03 <vermayash2003@gmail.com>
2024-01-07 16:27:13 +0530

    feat: add email and type as index

--- server/src/model/user.ts
+++ server/src/model/user.ts
@@ -1,27 +1,38 @@
 import { DataTypes } from 'sequelize';
 import sequelize from '../database/database.ts';
 
-const User = sequelize.define('User', {
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-    validate: {
-      isEmail: true
+const User = sequelize.define(
+  'User',
+  {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    type: {
+      type: DataTypes.ENUM('customer', 'seller'),
+      allowNull: false
     }
   },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  type: {
-    type: DataTypes.ENUM('customer', 'seller'),
-    allowNull: false
+  {
+    indexes: [
+      {
+        unique: true,
+        fields: ['email', 'type'],
+        name: 'unique-email-type'
+      }
+    ]
   }
-});
+);
 
 export default User;
