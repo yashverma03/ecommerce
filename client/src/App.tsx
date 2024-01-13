4beb3c95ecb7a6d122e6ebc8982c0a8a88e7ba99
yashverma03 <vermayash2003@gmail.com>
2024-01-13 14:22:20 +0530

    style: update design

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import { QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
+import './App.css';
 import { queryClient } from './utils/api';
 import SignUp from './components/SignUp/SignUp';
 import Login from './components/Login/Login';
