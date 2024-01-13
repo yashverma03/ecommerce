19c538ba7eecafd6d67a3d1ca79bb3065780b0ae
yashverma03 <vermayash2003@gmail.com>
2024-01-13 12:05:59 +0530

    chore: add component structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,15 +3,23 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { queryClient } from './utils/api';
 import SignUp from './components/SignUp/SignUp';
 import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
+        <Header />
+
         <Routes>
           <Route path='/sign-up' element={<SignUp />} />
           <Route path='/login' element={<Login />} />
+          <Route path='/' element={<Home />} />
         </Routes>
+
+        <Footer />
       </BrowserRouter>
     </QueryClientProvider>
   );
