23abdc2c78baeeb28b4a5199a69f607d85868b9e
yashverma03 <vermayash2003@gmail.com>
2024-02-25 16:38:06 +0530

    perf: add lazy loading

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,39 +1,44 @@
+import { lazy, Suspense } from 'react';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import './App.css';
 import queryClient from './config/queryClient';
-import SignUp from './pages/SignUp/SignUp';
-import Login from './pages/Login/Login';
-import Products from './pages/Products/Products';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
 import { store } from './store/store';
 import PrivateRoute from './routing/PrivateRoute';
-import Product from './pages/Product/Product';
-import Cart from './pages/Cart/Cart';
-import Order from './pages/Order/Order';
+import Spinner from './components/Spinner';
+
+const Header = lazy(async () => await import('./components/Header/Header'));
+const Footer = lazy(async () => await import('./components/Footer/Footer'));
+const SignUp = lazy(async () => await import('./pages/SignUp/SignUp'));
+const Login = lazy(async () => await import('./pages/Login/Login'));
+const Products = lazy(async () => await import('./pages/Products/Products'));
+const Product = lazy(async () => await import('./pages/Product/Product'));
+const Cart = lazy(async () => await import('./pages/Cart/Cart'));
+const Order = lazy(async () => await import('./pages/Order/Order'));
 
 const App = () => {
   return (
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
         <BrowserRouter>
-          <Header />
+          <Suspense fallback={<Spinner />}>
+            <Header />
 
-          <Routes>
-            <Route path='/sign-up' element={<SignUp />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/' element={<Products />} />
-            <Route path='/product/:id' element={<Product />} />
+            <Routes>
+              <Route path='/sign-up' element={<SignUp />} />
+              <Route path='/login' element={<Login />} />
+              <Route path='/' element={<Products />} />
+              <Route path='/product/:id' element={<Product />} />
 
-            <Route path='/' element={<PrivateRoute />}>
-              <Route path='/cart' element={<Cart />} />
-              <Route path='/order' element={<Order />} />
-            </Route>
-          </Routes>
+              <Route path='/' element={<PrivateRoute />}>
+                <Route path='/cart' element={<Cart />} />
+                <Route path='/order' element={<Order />} />
+              </Route>
+            </Routes>
 
-          <Footer />
+            <Footer />
+          </Suspense>
         </BrowserRouter>
       </QueryClientProvider>
     </Provider>
