1cf3fd836e96ae4e05dc91415f9c40e1c10e068c
yashverma03 <vermayash2003@gmail.com>
2024-03-02 13:11:14 +0530

    perf: add caching

---
+++ server/src/config/nodeCache.ts
@@ -0,0 +1,5 @@
+import NodeCache from 'node-cache';
+
+const cache = new NodeCache({ stdTTL: 3600 });
+
+export default cache;
--- server/src/controller/product.ts
+++ server/src/controller/product.ts
@@ -4,11 +4,22 @@ import {
   getProductByIdService,
   getProductsService
 } from '../service/product.ts';
+import cache from '../config/nodeCache.ts';
+import type { Categories, Product, Products } from '../utils/types.ts';
 
 export const getProducts = (req: Request, res: Response, next: NextFunction) => {
   const request = async () => {
     try {
-      const products = await getProductsService(req.query);
+      const { query } = req;
+      const cacheKey = 'products' + JSON.stringify(query);
+      const cachedProducts = cache.get<Products>(cacheKey);
+
+      if (cachedProducts != null) {
+        return res.status(200).json({ message: 'Products found (cached)', data: cachedProducts });
+      }
+
+      const products = await getProductsService(query);
+      cache.set(cacheKey, products);
       res.status(200).json({ message: 'Products found', data: products });
     } catch (error) {
       next(error);
@@ -22,7 +33,15 @@ export const getProductById = (req: Request, res: Response, next: NextFunction) 
   const request = async () => {
     try {
       const { id } = req.params;
+      const cacheKey = 'product' + id;
+      const cachedProduct = cache.get<Product>(cacheKey);
+
+      if (cachedProduct != null) {
+        return res.status(200).json({ message: 'Product found (cached)', data: cachedProduct });
+      }
+
       const product = await getProductByIdService(id);
+      cache.set(cacheKey, product);
       res.status(200).json({ message: 'Product found', data: product });
     } catch (error) {
       next(error);
@@ -35,7 +54,18 @@ export const getProductById = (req: Request, res: Response, next: NextFunction) 
 export const getCategories = (req: Request, res: Response, next: NextFunction) => {
   const request = async () => {
     try {
+      const cacheKey = 'categories';
+      const cachedCategories = cache.get<Categories>(cacheKey);
+
+      if (cachedCategories != null) {
+        return res.status(200).json({
+          message: 'Categories found (cached)',
+          data: cachedCategories
+        });
+      }
+
       const categories = await getCategoriesService();
+      cache.set(cacheKey, categories);
       res.status(200).json({ message: 'Categories found', data: categories });
     } catch (error) {
       next(error);
